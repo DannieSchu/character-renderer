fb5ea49c42d1fe109bd8692b9d2aec57f7860f90
Dannie Schumaker <draeschumaker@gmail.com>
2020-04-29 18:53:01 -0700

    create container for Detail component that hits API

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -4,23 +4,23 @@ import Character from '../Characters/Character.jsx';
 
 const Detail = ({ name, url, status, gender, born, description }) => {
   return (
-    <>
+    <section>
       <Character name={name} url={url} /> 
       <p>Status: {status}</p>
       <p>Gender: {gender}</p>
       <p>Date of Birth: {born}</p>
       <p>{description}</p>
-    </>
+    </section>
   );
 };
 
 Detail.propTypes = {
   name: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
-  status: PropTypes.string.isRequired,
-  gender: PropTypes.string.isRequired,
-  born: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired
+  status: PropTypes.string,
+  gender: PropTypes.string,
+  born: PropTypes.string,
+  description: PropTypes.string
 };
 
 export default Detail;
---
+++ src/containers/CharacterDetail/CharacterDetail.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import { useRouteMatch } from 'react-router-dom';
+import { getCharacter } from '../../services/XFiles.jsx';
+import Detail from '../../components/Detail/Detail.jsx';
+
+const CharacterDetail = () => {
+  const [character, setCharacter] = useState({});
+  const match = useRouteMatch('./characters/:name');
+
+  useEffect(() => {
+    getCharacter(match.params.name)
+      .then(fetchedCharacter => setCharacter(fetchedCharacter));
+  }, {});
+
+  return (
+    <>
+      <Detail character={character} />
+    </>
+  );
+};
+
+export default CharacterDetail;
